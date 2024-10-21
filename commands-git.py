9c136dcc73961b6b9faee1a4e56bbab45a2a3e1a
sandra-mastropasqua <sandra.mastropasqua@laplateforme.io>
2024-10-21 12:52:48 +0200

    job2

---
+++ commands-git.py
@@ -0,0 +1,9 @@
+Pour cloner un repository, je commence par créer un fichier, ensuite je copie la ligne de commande HTTPS.
+Jouvre GitBash, je cherche le fichier dans lequel je veux cloner mon repository.
+Puis je tape git clone avec la ligne de commande que jai copié précedemment
+Je tape entrée pour le créer. 
+
+Les 3 commandes de push : 
+- git add
+- git commit -m
+- git push 
